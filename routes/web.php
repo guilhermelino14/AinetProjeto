366153e26009fb39f8eff471671cc7ab5fe646b8
guilhermelino14 <lino000000@hotmail.com>
2021-04-14 21:58:00 +0100

    create user controller with resource

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class)->middleware('isAuthenticated');
