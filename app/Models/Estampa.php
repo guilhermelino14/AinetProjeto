93750220fcb62b2a69cdea35583c6ea34bae2b2f
guilhermelino14 <lino000000@hotmail.com>
2021-04-21 16:57:02 +0100

    add soft deletes

--- app/Models/Estampa.php
+++ app/Models/Estampa.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Estampa extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'id','cliente_id', 'categoria_id', 'nome', 'descricao', 'imagem_url','informacao_extra',
